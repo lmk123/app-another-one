5dfa6b1255c39747a652bbe200bd1a3161ab6777
lmk123 <milk.lee@qq.com>
2015-04-12 12:58:18 +0800

    完善样式

--- www/scripts/services/DataCacheService.js
+++ www/scripts/services/DataCacheService.js
@@ -7,39 +7,42 @@ define( [ '../app' ] , function ( app ) {
             if ( !window.requestFileSystem ) {
                 window.requestFileSystem = window.webkitRequestFileSystem;
             }
-            requestFileSystem( window.TEMPORARY , 500 * 1024 * 1024 /*500MB*/ , function ( fs ) {
-                var factory = {};
-                factory.get = function ( key ) {
-                    var def   = $q.defer() ,
-                        cache = memCache.get( key );
-                    if ( cache ) {
-                        def.resolve( cache );
-                    } else {
-                        fs.root.getFile( key + '.txt' , { create : false } , function ( fileEntry ) {
-                            fileEntry.file( function ( file ) {
-                                var reader = new FileReader();
-                                reader.onloadend = function () {
-                                    var result = JSON.parse( reader.result );
-                                    memCache.put( key , result );
-                                    def.resolve( result );
-                                };
-                                reader.readAsText( file );
+
+            navigator.webkitPersistentStorage.requestQuota( 500 * 1024 * 1024 /*500MB*/ , function ( grantedBytes ) {
+                requestFileSystem( PERSISTENT , grantedBytes , function ( fs ) {
+                    var factory = {};
+                    factory.get = function ( key ) {
+                        var def   = $q.defer() ,
+                            cache = memCache.get( key );
+                        if ( cache ) {
+                            def.resolve( cache );
+                        } else {
+                            fs.root.getFile( key + '.txt' , { create : false } , function ( fileEntry ) {
+                                fileEntry.file( function ( file ) {
+                                    var reader = new FileReader();
+                                    reader.onloadend = function () {
+                                        var result = JSON.parse( reader.result );
+                                        memCache.put( key , result );
+                                        def.resolve( result );
+                                    };
+                                    reader.readAsText( file );
+                                } );
+                            } , function () {
+                                def.resolve( false );
                             } );
-                        } , function () {
-                            def.resolve( false );
+                        }
+                        return def.promise;
+                    };
+                    factory.put = function ( key , value ) {
+                        memCache.put( key , value );
+                        fs.root.getFile( key + '.txt' , { create : true } , function ( fileEntry ) {
+                            fileEntry.createWriter( function ( witer ) {
+                                witer.write( new Blob( [ JSON.stringify( value ) ] , { type : 'text/plain' } ) );
+                            } );
                         } );
-                    }
-                    return def.promise;
-                };
-                factory.put = function ( key , value ) {
-                    memCache.put( key , value );
-                    fs.root.getFile( key + '.txt' , { create : true } , function ( fileEntry ) {
-                        fileEntry.createWriter( function ( witer ) {
-                            witer.write( new Blob( [ JSON.stringify( value ) ] , { type : 'text/plain' } ) );
-                        } );
-                    } );
-                };
-                def.resolve( factory );
+                    };
+                    def.resolve( factory );
+                } );
             } );
 
             return def.promise;
