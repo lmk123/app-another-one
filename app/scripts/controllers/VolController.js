0e3d858fc8cc3c51190662f83a0c12aa7bd89cdd
Milk Lee <milk.lee@qq.com>
2015-04-22 13:19:47 +0800

    将内容页底部的按钮改为 ng-click，而不是用指令来控制内容切换。 这是因为用 jQuery 监听 click 事件在手机端会有延迟

--- app/scripts/controllers/VolController.js
+++ app/scripts/controllers/VolController.js
@@ -3,20 +3,6 @@ define( [
     '../services/FetchVolFactory' ,
     '../services/NavService'
 ] , function ( app , $ ) {
-    app.directive( 'volPanel' , function () {
-        return {
-            link : function ( scope , element ) {
-                element.on( 'click' , 'a' , function ( e ) {
-                    var $this = $( this ) ,
-                        index = $this.index();
-                    e.preventDefault();
-                    $this.addClass( 'active' ).siblings( '.active' ).removeClass( 'active' );
-                    $( '#tabs-content > :eq(' + index + ')' ).addClass( 'active' ).siblings( '.active' ).removeClass( 'active' );
-                    $( 'html, body' ).animate( { scrollTop : 0 } , 500 );
-                } );
-            }
-        };
-    } );
     app.directive( 'volFav' , function () {
         return {
             link : function ( scope , element ) {
@@ -34,8 +20,15 @@ define( [
             };
             $scope.status = {
                 loading : true ,
-                showMenu : false
+                showMenu : false ,
+                current : 1
             };
+
+            $scope.switchTo = function ( number ) {
+                $scope.status.current = number;
+                $( 'html, body' ).animate( { scrollTop : 0 } , 500 );
+            };
+
             $scope.volData = {
                 id : $stateParams.id
             };
