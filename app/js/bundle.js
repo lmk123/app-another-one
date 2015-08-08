137e9962eb971fffe21dfc46175f4404ef9d85e2
Milk Lee <milk.lee@qq.com>
2015-08-08 15:52:23 +0800

    整理程序目录结构

---
+++ app/js/bundle.js
@@ -0,0 +1,18 @@
+(function () {
+    var concat = [
+            'vendor/Dexie.js' ,
+            'vendor/ionic/js/ionic.js' ,
+            'vendor/angular/angular.js' ,
+            'vendor/angular/angular-animate.js' ,
+            'vendor/angular/angular-sanitize.js' ,
+            'vendor/angular-ui/angular-ui-router.js' ,
+            'vendor/ionic/js/ionic-angular.js'
+        ] ,
+        s      = '';
+
+    concat.forEach( function ( url ) {
+        s += '<script src="' + url + '"></script>';
+    } );
+
+    document.write( s );
+}());
